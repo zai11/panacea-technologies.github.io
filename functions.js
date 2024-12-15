5b7c78a8a12d8a0ddb29d2d02c371b072bc7d40d
Zaichic Turner <zaipowell@rocketmail.com>
2024-12-16 00:03:51 +1000

    Finished Services part. Changed display background to use black dots.

--- functions.js
+++ functions.js
@@ -6,3 +6,10 @@ document.addEventListener("click", function(evnt){
     else
         $('#hamburger-menu').css('display', 'none');
 });
+
+function serviceClick (obj) {
+    console.log(obj);
+    console.log(obj.style.transform);
+    obj.style.transform = obj.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+    obj.style.boxShadow = obj.style.boxShadow === 'rgb(170, 170, 170) -5px 5px 5px' ? 'rgb(170, 170, 170) 5px 5px 5px' : 'rgb(170, 170, 170) -5px 5px 5px';
+}
